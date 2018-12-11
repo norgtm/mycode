61898a5593f76251bd93ced58ee10e4987102006
travis <travis.m.norgaard@aexp.com>
2018-12-10 22:14:15 -0500

    added input lab

---
+++ lab_input/input_ip.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Written by: Travis
+# Collect IP and Display it.
+
+## Collect it
+user_input = input('Please enter an IPv4 IP address:')
+## Display it
+print("You told me the IPv4 address is:" + user_input)
