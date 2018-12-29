710afaad6a8571e8e76fa27580e1f6af1e7420df
norgtm <wltn02@hotmail.com>
2018-12-28 17:23:36 -0700

    Added a line to test ssh url

--- lab_input/input_ip.py
+++ lab_input/input_ip.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # Written by: Travis
 # Collect IP and Display it.
+# Author: Travis Norgaard
 
 ## Collect it
 user_input = input('Please enter an IPv4 IP address: ')
 ## Display it
+print("You told me the IPv4 address is: " + user_input)
+## Display more 
 print("You told me the IPv4 address is: " + user_input)
